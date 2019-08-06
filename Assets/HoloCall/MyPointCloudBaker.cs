c5e07858af106eaead8acd62415ce781fbe4ca79
flufy3d <flufy3d@gmail.com>
2019-08-06 18:07:20 +0200

    add MyPointCloudBaker

---
+++ Assets/HoloCall/MyPointCloudBaker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPointCloudBaker : MonoBehaviour
+{
+
+    [Space]
+    [SerializeField] RenderTexture _colorMap = null;
+    [SerializeField] RenderTexture _positionMap = null;
+
+    [SerializeField] ComputeShader _compute = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
