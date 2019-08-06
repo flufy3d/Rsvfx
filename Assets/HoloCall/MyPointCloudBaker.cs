54dc77db307c0a2a2732303e6fa144454d67c0ba
flufy3d <flufy3d@gmail.com>
2019-08-06 23:09:43 +0200

    now the compute shader is work

--- Assets/HoloCall/MyPointCloudBaker.cs
+++ Assets/HoloCall/MyPointCloudBaker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
 
 public class MyPointCloudBaker : MonoBehaviour
 {
@@ -11,15 +12,47 @@ public class MyPointCloudBaker : MonoBehaviour
 
     [SerializeField] ComputeShader _compute = null;
 
+    RenderTexture _tempColorMap;
+    RenderTexture _tempPositionMap;
+
+    int2 _dimensions;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _dimensions = math.int2(640, 480);
+
+        if (_tempColorMap == null)
+        {
+            _tempColorMap = new RenderTexture(
+                _dimensions.x, _dimensions.y, 0,
+                RenderTextureFormat.ARGB32
+            );
+            _tempColorMap.enableRandomWrite = true;
+            _tempColorMap.Create();
+        }
+
+        if (_tempPositionMap == null)
+        {
+            _tempPositionMap = new RenderTexture(
+                _dimensions.x, _dimensions.y, 0,
+                RenderTextureFormat.ARGBHalf
+            );
+            _tempPositionMap.enableRandomWrite = true;
+            _tempPositionMap.Create();
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _compute.SetTexture(0, "ColorMap", _tempColorMap);
+        _compute.SetTexture(0, "PositionMap", _tempPositionMap);
+        _compute.Dispatch(0, 640 / 8, 480 / 8, 1);
+
+        Graphics.CopyTexture(_tempColorMap, _colorMap);
+        Graphics.CopyTexture(_tempPositionMap, _positionMap);
     }
 }
